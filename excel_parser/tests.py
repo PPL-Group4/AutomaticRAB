141ff493ea896562252e6455bfafc7d5628173a8
syifa-ananda <syifaananda36@gmail.com>
2025-09-17 20:45:20 +0700

    [RED] Add failing tests for ExcelSniffer

--- excel_parser/tests.py
+++ excel_parser/tests.py
@@ -2,6 +2,8 @@ from django.test import TestCase
 from django.core.files.uploadedfile import SimpleUploadedFile
 from django.core.exceptions import ValidationError
 from excel_parser.validators import validate_excel_file   
+from excel_parser.services import ExcelSniffer   
+from openpyxl import Workbook
 
 class FileValidatorTests(TestCase):
     def test_accepts_xlsx_file(self):
@@ -51,3 +53,71 @@ class FileValidatorTests(TestCase):
             validate_excel_file(file)
         except ValidationError:
             self.fail("File kosong dengan tipe valid harus diterima")
+
+try:
+    import xlrd
+    from xlwt import Workbook as XlsWorkbook
+except ImportError:
+    xlrd = None
+    XlsWorkbook = None
+
+
+class ExcelSnifferTests(TestCase):
+    def test_valid_xlsx_file(self):
+        file_path = "/tmp/valid.xlsx"
+        wb = Workbook()
+        wb.save(file_path)
+
+        sniffer = ExcelSniffer()
+        self.assertIsNone(sniffer.is_valid(file_path))
+
+    def test_valid_empty_xlsx_file(self):
+        file_path = "/tmp/empty.xlsx"
+        wb = Workbook()
+        wb.save(file_path)
+
+        sniffer = ExcelSniffer()
+        self.assertIsNone(sniffer.is_valid(file_path))
+
+    def test_invalid_xlsx_file(self):
+        file_path = "/tmp/invalid.xlsx"
+        with open(file_path, "wb") as f:
+            f.write(b"not really excel")
+
+        sniffer = ExcelSniffer()
+        with self.assertRaises(ValidationError):
+            sniffer.is_valid(file_path)
+
+    def test_fake_xlsx_with_wrong_content(self):
+        file_path = "/tmp/fake.xlsx"
+        with open(file_path, "w") as f:
+            f.write("Hello world")
+
+        sniffer = ExcelSniffer()
+        with self.assertRaises(ValidationError):
+            sniffer.is_valid(file_path)
+
+    def test_valid_xls_file(self):
+        if not XlsWorkbook:
+            self.skipTest("xlrd/xlwt not installed")
+
+        file_path = "/tmp/valid.xls"
+        wb = XlsWorkbook()
+        sheet = wb.add_sheet("Sheet1")
+        sheet.write(0, 0, "Hello XLS")
+        wb.save(file_path)
+
+        sniffer = ExcelSniffer()
+        self.assertIsNone(sniffer.is_valid(file_path))
+
+    def test_invalid_xls_file(self):
+        if not xlrd:
+            self.skipTest("xlrd not installed")
+
+        file_path = "/tmp/invalid.xls"
+        with open(file_path, "wb") as f:
+            f.write(b"not really an xls")
+
+        sniffer = ExcelSniffer()
+        with self.assertRaises(ValidationError):
+            sniffer.is_valid(file_path)
