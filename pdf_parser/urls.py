6b65d09d714b50c24461529589a81968a8a128e5
syifa-ananda <syifaananda36@gmail.com>
2025-10-01 15:58:57 +0700

    [GREEN] implement rab_converted_pdf endpoint with template and url mapping

--- pdf_parser/urls.py
+++ pdf_parser/urls.py
@@ -4,9 +4,5 @@ from . import views
 app_name = "pdf_parser"
 
 urlpatterns = [
-    path("upload/", views.upload_pdf, name="upload_pdf"),
-    path("preview/", views.preview_pdf, name="preview_pdf"),
-    path("parse", views.parse_pdf_view, name="parse_pdf"),
     path("rab_converted/", views.rab_converted_pdf, name="rab_converted_pdf"),
-
 ]
