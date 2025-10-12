caffed738c7dc6dc4c5c8dc150d8d66cef3686ae
thataruby <thata@dfi.id>
2025-10-12 23:57:07 +0700

    [GREEN] Added language detection to skip if text is already indonesian

--- automatic_job_matching/service/translation_service.py
+++ automatic_job_matching/service/translation_service.py
@@ -1,5 +1,5 @@
-import logging
 from deep_translator import GoogleTranslator
+from langdetect import detect, LangDetectException
 
 class TranslationService:
     def __init__(self):
@@ -8,7 +8,16 @@ class TranslationService:
     def translate_to_indonesian(self, text: str) -> str:
         if not text:
             return ""
+
         try:
+            try:
+                lang = detect(text)
+            except LangDetectException:
+                lang = "unknown"
+
+            if lang == "id":
+                return text
+
             return self.translator.translate(text)
         except Exception:
             return text
