237660983bf321fde93b025fa8b614d7b5161b51
GhazaFadhlilbaqi <fadhlilbaqimuhammadghaza@gmail.com>
2025-10-20 19:25:26 +0700

    [CHORE] Created Dockerfile and CD script to deploy on AWS Server

--- AutomaticRAB/settings.py
+++ AutomaticRAB/settings.py
@@ -27,9 +27,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv("DEBUG", "False") == "True" 
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1","controversial-annmarie-izzydharma-9d67c1b0.koyeb.app"]
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "localhost").split(",")
 
 
 # Application definition
