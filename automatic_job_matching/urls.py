81983a5657dc963470aa6985f519ded17e3081e9
thataruby <thata@dfi.id>
2025-10-21 23:57:00 +0700

    [REFACTOR] Deleted redundant search & suggest

--- automatic_job_matching/urls.py
+++ automatic_job_matching/urls.py
@@ -1,8 +1,7 @@
 from django.urls import path
-from .views import match_best_view, job_matching_page, suggest_matches_view
+from .views import match_best_view, job_matching_page
 
 urlpatterns = [
     path("match-best/", match_best_view, name="match-best"),
     path("job-matching/", job_matching_page, name="job-matching"),
-    path("suggest/", suggest_matches_view, name="match-suggest"),
 ]
